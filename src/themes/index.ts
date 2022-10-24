d4821a77cfca0a8bb9d374e721bc4681d3d84cab
Junya <93970227+junyan21@users.noreply.github.com>
2022-10-24 19:11:25 +0900

    Setup Initial Strybook conf

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from "./colors";
+import fontSizes from "./fontSizes";
+import letterSpacings from "./letterSpacings";
+import lineHeights from "./lineHeights";
+import space from "./spaces";
+
+export const theme = {
+    space,
+    fontSizes,
+    letterSpacings,
+    lineHeights,
+    colors
+} as const
