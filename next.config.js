260fbf14e195151b4607b549867f32007871234b
Junya <93970227+junyan21@users.noreply.github.com>
2022-10-24 19:35:50 +0900

    Setup some libraries and jest environment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    // StyledComponentsの有効化
-    styledComponents: true,
-  }
+  compiler: (() => {
+    let compilerConfig = {
+      // StyledComponentsの有効化
+      styledComponents: true,
+    }
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
